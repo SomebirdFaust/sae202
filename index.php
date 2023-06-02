bae40bb1085a756b5c8d6ccfb32fde2a2fcdb199
LeaneBerlo <leane.berlo@gmail.com>
2023-06-02 12:04:45 +0200

    test

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ $mabd = new PDO('mysql:host=localhost;dbname=sae202;charset=UTF8;', 'sae202admin
 $mabd->query('SET NAMES utf8;');
 $req = "SELECT * FROM  utilisateur ";
 $resultat = $mabd->query($req);
-
+//
 ?>
