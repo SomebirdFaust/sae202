de9777ef93e9fd257f538e2bb2ddc33a218502b3
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-06 16:32:48 +0200

    page connexion quasi marche

--- connexion.php
+++ connexion.php
@@ -7,7 +7,8 @@ require 'header.php';
  <h1>Connexion</h1> 
  <form action="admin/connexionVerif.php" method="post"> 
  Adresse e-mail : <input type="text" name="email" /><br />  
- Mot de passe : <input type="password" name="mdp" /><br />  <input type="submit" value="Envoyer"> 
+ Mot de passe : <input type="password" name="mdp" /><br />  
+ <input type="submit" value="Envoyer"> 
  </form> 
 
     <?php
--- inscription.php
+++ inscription.php
@@ -17,6 +17,13 @@ require 'header.php';
         <label for="prenom">Prénom :</label>
         <input type="text" id="prenom" name="prenom" required><br><br>
 
+        <label for="genre">Genre :</label>
+  <select name="genre" id="genre" required>
+    <option value="homme">Homme</option>
+    <option value="femme">Femme</option>
+    <option value="autre">Autre</option>
+  </select>
+
         <label for="email">Email :</label>
         <input type="email" id="email" name="email" required><br><br>
 
