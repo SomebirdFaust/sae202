da899773ff030dc72b04a5f5103922e1a559d366
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-06 16:34:07 +0200

    Add files via upload

--- admin/connexionVerif.php
+++ admin/connexionVerif.php
@@ -1,17 +1,21 @@
 <?php
-require 'lib.inc.php';
-$email = $_POST['email'];
-$mdp = $_POST['mdp'];
-$mabd = connexionBD();
-$req = 'SELECT * FROM utilisateurs WHERE user_mail = "'.$email.'"';
-$resultat = $mabd->query($req);
-$lignes_resultat = $resultat->rowCount();
+    session_start();
+    require 'lib.inc.php';
+
+    $email = $_POST['email'];
+    $mdp = $_POST['mdp'];
+    
+    $mabd = connexionBD();
+    $req = 'SELECT * FROM utilisateurs WHERE user_mail = "'.$email.'"';
+
+    $resultat = $mabd->query($req);
+    $lignes_resultat = $resultat->rowCount();
 
 if ($lignes_resultat > 0) {
     $ligne = $resultat->fetch(PDO::FETCH_ASSOC);
     if (password_verify($mdp, $ligne['user_mdp'])) {
 
-        $_SESSION['user_email'] = $ligne['user_mail'];
+        $_SESSION['email'] = $ligne['user_mail'];
 
         header('location:../profil.php');
 
