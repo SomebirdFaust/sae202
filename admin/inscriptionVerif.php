ae3abd8a07f5f82c56237f37915a0cf356f6d917
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-07 17:07:32 +0200

    Add files via upload

--- admin/inscriptionVerif.php
+++ admin/inscriptionVerif.php
@@ -18,17 +18,24 @@ try {
     $result = $req->fetch(PDO::FETCH_ASSOC);
 
     if ($result['count'] > 0) {
-        // Utilisateur déjà existant, afficher un message d'erreur ou rediriger vers une autre page
+        // Utilisateur existant, afficher un message d'erreur ou rediriger vers une autre page
         echo "Cet email est déjà utilisé, veuillez vous connecter !";
         header('location: ../inscription.php?erreur=1');
         exit();
     } else {
+        // Sécurisation du mot de passe avec un hash
         $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
         $req = $mabd->prepare('INSERT INTO utilisateurs (user_nom, user_prenom, user_mail, user_mdp, user_genre, user_bio, user_car) VALUES (:nom, :prenom, :email, :mdp, :genre, :bio, :car)');
         $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':email' => $email, ':mdp' => $mdp_hash, ':genre' => $genre, ':bio' => $bio, ':car' => $detailsVoiture));
 
-        // Afficher un message de succès ou rediriger vers une autre page
-        echo "Inscription réussie !";
+        // Connexion automatique de l'utilisateur
+        $user = grab_user($mabd, $email);
+        if ($user) {
+            session_start();
+            $_SESSION['email'] = $user['user_mail'];
+        }
+
+        // Rediriger vers une autre page
         header('location: ../index.php?succes=1');
         exit();
     }
--- admin/lib.inc.php
+++ admin/lib.inc.php
@@ -1,8 +1,9 @@
 <?php
+//voir si une session existe déjà, sinn la créer
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
+//voir si la bd est déjà co, sinon la connecter
 if (!function_exists('connexionBD')) {
     function connexionBD()
     {
@@ -13,23 +14,38 @@ if (!function_exists('connexionBD')) {
         return $mabd;
     }
 }
-
+//voir si la bd est déjà déco, sinon la déconnecter
 if (!function_exists('deconnexionBD')) {
     function deconnexionBD(&$mabd)
     {
         unset($mabd);
     }
 }
-?>
 
-<?php
 
-function user_deco()
-{
-    $_SESSION=array();
+function deconnecterUtilisateur() {
+    session_start();
 
+    // Détruire toutes les variables de session
+    $_SESSION = array();
+
+    // Effacer le cookie de session
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-42000, '/');
+    }
+
+    // Détruire la session
     session_destroy();
-
-    header('location:index.php');
 }
-?>
+
+// Fonction pour récupérer les informations de l'utilisateur connecté
+function userConnect($mabd) {
+    if (isset($_SESSION['user_id'])) {
+        $userId = $_SESSION['user_id'];
+        $req = $mabd->prepare('SELECT * FROM utilisateurs WHERE user_id = :user_id');
+        $req->execute(array(':user_id' => $userId));
+        $user = $req->fetch(PDO::FETCH_ASSOC);
+        return $user;
+    }
+    return null;
+}
