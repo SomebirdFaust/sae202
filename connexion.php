15d4b577098eb466ab954a083d38cfd5abc9d01c
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-07 14:02:10 +0200

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -2,24 +2,15 @@
 require 'admin/lib.inc.php';
 require 'header.php';
 ?> 
-
-<<<<<<< HEAD
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Connexion</title>
 </head>
 <body>
-        <div id="contenu"> 
-                <h1>Connexion</h1> 
-                <form action="admin/connexionVerif.php" method="post"> 
-                Adresse e-mail : <input type="text" name="email" /><br />  
-                Mot de passe : <input type="password" name="mdp" /><br />  <input type="submit" value="Envoyer"> 
-                </form> 
-=======
  <div id="contenu"> 
  <h1>Connexion</h1> 
  <form action="admin/connexionVerif.php" method="post"> 
@@ -27,20 +18,19 @@ require 'header.php';
  Mot de passe : <input type="password" name="mdp" /><br />  
  <input type="submit" value="Envoyer"> 
  </form> 
->>>>>>> da899773ff030dc72b04a5f5103922e1a559d366
 
-                <?php
-                        if (!empty($_SESSION['erreur'])) {
-                        echo $_SESSION['erreur'];
-                        unset ($_SESSION['erreur']);
-                        }
-                        //var_dump($_SESSION);
-                ?>
+    <?php
+            if (!empty($_SESSION['erreur'])) {
+            echo $_SESSION['erreur'];
+            unset ($_SESSION['erreur']);
+            }
+            //var_dump($_SESSION);
+    ?>
 
-         </div> 
+ </div> 
+
 </body>
 </html>
-
 
 <?php 
 require 'footer.php';
--- profil.php
+++ profil.php
@@ -2,55 +2,7 @@
 require 'admin/lib.inc.php';
 require 'header.php';
 ?> 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
-</head>
-<body>
-
-
-<?php
-
-$mabd = new PDO('mysql:host=localhost;dbname=sae202;charset=UTF8;', 'sae202admin', 'WW3dbpasswd202');
-$mabd->query('SET NAMES utf8;');
-
-$req = "SELECT * FROM utilisateurs";
-$resultat = $mabd->query($req);
-
-//INFOS PERSONNELLES DE L'UTILISATEUR
-//POTENTIELLEMENT UTILISER DES VARIABLES DE SESSION POUR RÉCUPÉRER LES INFOS DE CONNEXION 
-//POUR SAVOIR LES INFOS DE QUEL USER AFFICHER (AVEC SON ID)
-//MERCI FAUST :)
-
-    echo '<div id="infos_profil">' ;
-        echo '<img src="img/avatar.png" alt="avatar">';
-        //echo '<p>' . ['user_prenom'] . '</p>';
-        //echo '<p>' . ['user_nom'] . '</p>';
-        //echo '<p>' . ['user_mail'] . '</p>';
-    echo '</div>';
-
-//BIOGRAPHIE DE L'UTILISATEUR
-//MEME PRINCIPE
-    echo '<div id="bio_profil">' ;
-        //echo '<p>' . ['user_bio'] . '</p>';
-    echo '</div>' ;
-
-//VOITURE DE L'UTILISATEUR
-//TOUJOURS PAREIL
-echo '<div id="bio_profil">' ;
-    //echo '<p>' . ['user_car'] . '</p>';
-echo '</div>' ;
 
 
 
-require 'footer.php';
-?>
-
-</body>
-</html>
-
-   
+    <?php require 'footer.php'; ?>
