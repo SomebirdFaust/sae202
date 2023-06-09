6d7a3a84c448f0714838619df9bfea13e6bfbb49
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-09 11:22:08 +0200

    Update validModifProfil.php

--- admin/validModifProfil.php
+++ admin/validModifProfil.php
@@ -10,22 +10,32 @@ $voiture = $_POST['voiture'];
 
 try {
     $mabd = connexionBD();
+    $req = $mabd->prepare('SELECT COUNT(*) as count FROM utilisateurs WHERE user_mail = :email');
+    $req->execute(array(':email' => $email));
+    $result = $req->fetch(PDO::FETCH_ASSOC);
 
-    // Mettre à jour les informations de l'utilisateur
-    if (!empty($mdp)) {
-        $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
-        $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_mdp = :mdp, user_bio = :bio, user_car = :car, user_mail = :nouveau_email WHERE user_mail = :email');
-        $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':mdp' => $mdp_hash, ':bio' => $bio, ':car' => $voiture, ':nouveau_email' => $email, ':email' => $user['user_mail']));
+    if ($result['count'] > 0) {
+        if (!empty($mdp)) {
+            $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
+            $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_mdp = :mdp, user_bio = :bio, user_car = :car WHERE user_mail = :email');
+            $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':mdp' => $mdp_hash, ':bio' => $bio, ':car' => $voiture, ':email' => $email));
+        } else {
+            $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_bio = :bio, user_car = :car WHERE user_mail = :email');
+            $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':bio' => $bio, ':car' => $voiture, ':email' => $email));
+        }
+
+        header('location: ../profil.php?succes=1');
+        exit();
     } else {
-        $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_bio = :bio, user_car = :car, user_mail = :nouveau_email WHERE user_mail = :email');
-        $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':bio' => $bio, ':car' => $voiture, ':nouveau_email' => $email, ':email' => $user['user_mail']));
+        echo "Utilisateur non trouvé !";
+        header('location: ../modifProfil.php?erreur=1');
+        exit();
     }
-
-    header('location: ../profil.php?succes=1');
-    exit();
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 
 deconnexionBD($mabd);
 ?>
+
+
