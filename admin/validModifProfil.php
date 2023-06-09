e2db0596230eb21217647883c8e8b7d8638e6fa2
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-09 10:54:11 +0200

    Update validModifProfil.php

--- admin/validModifProfil.php
+++ admin/validModifProfil.php
@@ -17,9 +17,14 @@ try {
 
     if ($result['count'] > 0) {
         // Utilisateur existant, mettre à jour les informations
-        $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
-        $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_mdp = :mdp, user_bio = :bio, user_car = :car WHERE user_mail = :email');
-        $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':mdp' => $mdp_hash, ':genre' => $genre, ':bio' => $bio, ':car' => $voiture, ':email' => $email));
+        if (!empty($mdp)) {
+            $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
+            $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_mdp = :mdp, user_bio = :bio, user_car = :car WHERE user_mail = :email');
+            $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':mdp' => $mdp_hash, ':bio' => $bio, ':car' => $voiture, ':email' => $email));
+        } else {
+            $req = $mabd->prepare('UPDATE utilisateurs SET user_nom = :nom, user_prenom = :prenom, user_bio = :bio, user_car = :car WHERE user_mail = :email');
+            $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':bio' => $bio, ':car' => $voiture, ':email' => $email));
+        }
 
         header('location: ../profil.php?succes=1');
         exit();
@@ -34,4 +39,3 @@ try {
 
 deconnexionBD($mabd);
 ?>
-
