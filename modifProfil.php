2f362eef109b2ec07fe4dd0c6795f16237aadaf8
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-09 10:25:53 +0200

    correction modifProfil.php

--- modifProfil.php
+++ modifProfil.php
@@ -17,11 +17,12 @@ $mabd = connexionBD();
 $user = grab_user($mabd);
 
 if ($user) {
-    echo '<form action="admin/validModifProfil.php" method="post">';
+    echo '<form action="validModifProfil.php" method="post">';
     echo '<div id="infos_profil">';
-    echo '<input type="text" name="prenom" value="' . ucfirst($user['user_prenom']) . '"><br />';
-    echo '<input type="text" name="nom" value="' . ucfirst($user['user_nom']) . '"><br />';
-    echo '<input type="email" name="email" value="' . $user['user_mail'] . '"><br />';
+    echo 'Prenom <input type="text" name="prenom" value="' . ucfirst($user['user_prenom']) . '"><br />';
+    echo 'Nom <input type="text" name="nom" value="' . ucfirst($user['user_nom']) . '"><br />';
+    echo 'Email <input type="email" name="email" value="' . $user['user_mail'] . '"><br />';
+    echo 'Mot de Passe <input type="text" name="mdp" value="' . $user['user_mdp'] . '"><br />';
     echo '</div>';
 
     echo '<div id="bio_profil">';
@@ -46,3 +47,5 @@ require 'footer.php';
 ?>
 </body>
 </html>
+
+
