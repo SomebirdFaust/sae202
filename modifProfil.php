cb92bf2feb66a4273cdee72bc092deaab2420985
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-09 10:09:54 +0200

    modifications modifProfil.php

--- modifProfil.php
+++ modifProfil.php
@@ -1,46 +1,48 @@
-<?php 
-        require 'admin/lib.inc.php';
-        require 'header.php';
-    ?> 
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Modification Profil</title>
-    </head>
-    <body>
-    <?php
+<?php
+require 'admin/lib.inc.php';
+require 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modification Profil</title>
+</head>
+<body>
+<?php
 $mabd = connexionBD();
 $user = grab_user($mabd);
 
 if ($user) {
+    echo '<form action="validModifProfil.php" method="post">';
     echo '<div id="infos_profil">';
-    echo '<img src="img/avatar.png" alt="avatar">';
-    echo ucfirst($user['user_prenom']) . '<br />' . "\n";
-    echo ucfirst($user['user_nom']) . '<br />' . "\n";
-    echo $user['user_mail'] . '<br />';
+    echo '<input type="text" name="prenom" value="' . ucfirst($user['user_prenom']) . '"><br />';
+    echo '<input type="text" name="nom" value="' . ucfirst($user['user_nom']) . '"><br />';
+    echo '<input type="email" name="email" value="' . $user['user_mail'] . '"><br />';
     echo '</div>';
 
     echo '<div id="bio_profil">';
-    echo ucfirst($user['user_bio']) . '<br />' . "\n";
+    echo '<textarea name="bio">' . ucfirst($user['user_bio']) . '</textarea><br />';
     echo '</div>';
 
     echo '<div id="voiture_profil">';
-    echo ucfirst($user['user_car']) . '<br />' . "\n";
+    echo '<input type="text" name="voiture" value="' . ucfirst($user['user_car']) . '"><br />';
     echo '</div>';
+
+    echo '<input type="submit" value="Enregistrer les modifications">';
+    echo '</form>';
 } else {
     echo "Vous n'êtes pas connecté(e) !";
 }
 ?>
-    <hr />
-</div>
+
+<hr />
 
 <?php
-    require 'footer.php';
-    ?>
+require 'footer.php';
+?>
 </body>
 </html>
-
-
