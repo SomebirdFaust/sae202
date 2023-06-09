f2a4eca1279543f58a04bb37f2f4054c789b2372
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-09 12:22:01 +0200

    Update connexionVerif.php

--- connexionVerif.php
+++ connexionVerif.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'lib.inc.php';
+require 'admin/lib.inc.php';
 
 // Vérifier si l'email et le mdp sont corrects
 if (isset($_POST['email']) && isset($_POST['mdp'])) {
@@ -17,16 +17,16 @@ if (isset($_POST['email']) && isset($_POST['mdp'])) {
     $ligne = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($ligne && password_verify($mdp, $ligne['user_mdp'])) {
         $_SESSION['user_id'] = $ligne['user_id']; // Ajouter cette ligne pour enregistrer l'ID de l'utilisateur connecté
-        header('Location: ../index.php');
+        header('Location: index.php');
         exit();
     } else {
         $_SESSION['erreur'] = '<p class="erreur">Le mot de passe saisi est incorrect.</p>';
-        header('Location: ../connexion.php');
+        header('Location: connexion.php');
         exit();
     }
 } else {
     $_SESSION['erreur'] = '<p class="erreur">Veuillez fournir une adresse email et un mot de passe.</p>';
-    header('Location: ../connexion.php');
+    header('Location: connexion.php');
     exit();
 }
 
