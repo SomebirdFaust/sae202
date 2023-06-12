748a55792bbdd7aae876074b581381e16236baf5
Faust Gaulard <126809005+SomebirdFaust@users.noreply.github.com>
2023-06-12 16:24:24 +0200

    suppr des commentaires

--- deconnexion.php
+++ deconnexion.php
@@ -3,11 +3,9 @@ require 'admin/lib.inc.php';
 
 deconnecterUtilisateur();
 
-//détruire la session
 session_start();
 session_destroy();
 
-//rediriger vers la page d'accueil ou une autre page après la déconnexion
 header('Location: index.php');
 exit();
 ?>
