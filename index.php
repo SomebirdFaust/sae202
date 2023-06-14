6de14a24a8df0b6ed13e536e031ac9d82b767b3c
LeaneBerlo <leane.berlo@gmail.com>
2023-06-14 15:21:58 +0200

    page accueil tel ok, en attente de faust php

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <?php require 'header.php';?>
 
 <h1 id="accueil_logo">Car & Cie</h1>
-<div id="contenu">
-    <h1>Bienvenue sur le site de Car&Cie !</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor suscipit ad molestiae assumenda odio blanditiis eos eaque perspiciatis iure, ut temporibus laudantium asperiores sunt laborum debitis commodi id, dolorum sed.</p>
 
+<div id="recherche_trajet">
+    <!-- champs à faire apparaitre : 
+        départ
+        destination
+        date
+        bouton submit
+    -->
 </div>
+<div id="video">
+    <iframe src="https://www.youtube.com/embed/I_UktiOb_7c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+<div id="questions">
+    <h1>FAQ</h1>
+        <div class="question">
+            <h3>Pourquoi faire du covoiturage?</h3><br>
+            <p>En tant qu’étudiant, on est souvent confronté à des contraintes 
+            budgétaires, le covoiturage nous permet donc de faire des économies 
+            tout en réduisant notre empreinte carbone et en rencontrant de 
+            nouvelles personnes.</p>
+        </div>
+        <div class="question">
+            <h3>Combien coûte un trajet en covoiturage ?</h3>
+            <p>Notre site est entièrement gratuit! Les conducteurs sont gratifiés 
+            par votre bonne humeur et le sentiment d'être utile, 
+            tout en préservant l'environnement!</p>
+        </div>
+        <div class="question">
+            <h3>Comment proposer un trajet de covoiturage ?</h3>
+            <p>Pour proposer un trajet, vous devez disposer d'un compte utilisateur 
+            et être connecté. Si vous n'en avez pas encore, il vous suffit d'en 
+            créer un en vous rendant sur la page "inscription" dans le menu. 
+            Ensuite, vous n'aurez qu'à vous rendre sur la page "publier un trajet" 
+            également situé dans le menu, et de remplir les informations requises.
+            Rien de plus simple!</p>
+        </div>
+</div>
+
 
 <?php require 'footer.php'; ?>
 
