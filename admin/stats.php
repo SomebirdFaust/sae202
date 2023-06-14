65aa8c3c5f92671891cf61633c9aff8bbb761a25
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-14 10:50:23 +0200

    modified:   admin/stats.php

--- admin/stats.php
+++ admin/stats.php
@@ -5,11 +5,18 @@ require 'lib.inc.php';
 $mabd = connexionBD();
 $mabd->query('SET NAMES utf8;');
 $req = "SELECT COUNT(traj_id) AS nombre_trajets FROM trajets";
-$resultat = $mabd->query($req);
-$row = $resultat->fetch(PDO::FETCH_ASSOC);
+$tresult = $mabd->query($req);
+$row = $tresult->fetch(PDO::FETCH_ASSOC);
 $nombreTrajets = $row['nombre_trajets'];
 
 echo "Nombre de trajets : " . $nombreTrajets;
+?>
+<?php
+$req = "SELECT COUNT(user_id) AS nombre_utilisateurs FROM utilisateurs";
+$uresult = $mabd->query($req);
+$row = $uresult->fetch(PDO::FETCH_ASSOC);
+$nombreUtilisateurs = $row['nombre_utilisateurs'];
 
+echo "Nombre d'utilisateurs : " . $nombreUtilisateurs;
 ?>
 
