b1ce8a11b8b020de8c83fba0bc880f72c97b206e
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-14 10:18:59 +0200

    modified:   admin/stats.php modified:   index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php require 'header.php'; ?>
+<?php require 'header.php';
+      require 'admin/lib.inc.php'; ?>
 
 
 <div id="contenu">
-            <h1>Bienvenue sur le site de Car&Cie !</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor suscipit ad molestiae assumenda odio blanditiis eos eaque perspiciatis iure, ut temporibus laudantium asperiores sunt laborum debitis commodi id, dolorum sed.</p>
-        </div>
+    <h1>Bienvenue sur le site de Car&Cie !</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor suscipit ad molestiae assumenda odio blanditiis eos eaque perspiciatis iure, ut temporibus laudantium asperiores sunt laborum debitis commodi id, dolorum sed.</p>
 
+</div>
 
 <?php require 'footer.php'; ?>
 
