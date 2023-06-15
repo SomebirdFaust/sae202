67f46bae866428adf4fd12eb8ddf3930f68f1aec
LeaneBerlo <leane.berlo@gmail.com>
2023-06-16 01:03:45 +0200

    inscription ok

--- inscription.php
+++ inscription.php
@@ -58,41 +58,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <h1>Inscription</h1>
       <img src="img/avatar.png" alt="avatar">
       <form action="" method="post">
-        <label for="prenom">Prénom*</label>
-        <input class="input" type="text" id="prenom" name="prenom" required><br><br>
+        <label for="prenom">Prénom*</label><br>
+        <input class="input input_pc" type="text" id="prenom" name="prenom" required><br><br>
 
-        <label for="nom">Nom*</label>
-        <input class="input" type="text" id="nom" name="nom" required><br><br>
+        <label for="nom">Nom*</label><br>
+        <input class="input input_pc" type="text" id="nom" name="nom" required><br><br>
 
-        <label for="genre">Pronoms*</label>
-        <select class="input" name="genre" id="genre" required>
+        <label for="genre">Pronoms*</label><br>
+        <select class="input input_pc" name="genre" id="genre" required>
             <option value="il">Il</option>
             <option value="elle">Elle</option>
             <option value="iel">Iel</option>
         </select><br><br>
 
-        <label for="email">Email*</label>
-        <input class="input" type="email" id="email" name="email" required><br><br>
+        <label for="email">Email*</label><br>
+        <input class="input input_pc" type="email" id="email" name="email" required><br><br>
 
-        <label for="mdp">Mot de passe*</label>
-        <input class="input" type="password" id="mdp" name="mdp" required><br><br>
+        <label for="mdp">Mot de passe*</label><br>
+        <input class="input input_pc" type="password" id="mdp" name="mdp" required><br><br>
 
         <label for="voiture">Avez-vous une voiture ?*</label><br>
         <div id="radio_voiture">
-    <div id="oui">
-        <input class="input" type="radio" name="voiture" value="oui" onclick="showTextField()">
-        <label for="voiture">Oui</label>
-    </div>
-    <div id="non">
-        <input class="input" type="radio" name="voiture" value="non" onclick="hideTextField()" checked><br>
-        <label for="voiture">Non</label>
-    </div>
-</div>
+            <div id="oui">
+                <input class="input input_pc" type="radio" name="voiture" value="oui" onclick="showTextField()">
+                <label for="voiture">Oui</label>
+            </div>
+            <div id="non">
+                <input class="input input_pc" type="radio" name="voiture" value="non" onclick="hideTextField()" checked><br>
+                <label for="voiture">Non</label>
+            </div>
+        </div>
 
 
         <div id="detailsVoiture" style="display: none;">
           <label for="detailsVoiture">Précisez la marque, la couleur et le modèle de votre voiture :*</label><br>
-          <input class="input" type="text" name="detailsVoiture" placeholder="Marque, couleur, modèle"><br><br>
+          <input class="input input_pc" type="text" name="detailsVoiture" placeholder="Marque, couleur, modèle"><br><br>
         </div>
 
         <div id="inscription_submit">
