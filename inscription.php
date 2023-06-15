5b325487e360b07859e2285a41d610d756f2310e
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-15 19:55:55 +0200

    v jsp cb

--- inscription.php
+++ inscription.php
@@ -1,5 +1,5 @@
 <?php
-require 'header.php';
+require 'admin/lib.inc.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom = ucfirst($_POST['nom']);
@@ -24,13 +24,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } else {
             $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT, ['cost' => 12]);
 
-            $car = !empty($voiture) ? $voiture : null;
+            if (empty($voiture)) {
+                // Si aucun modèle de voiture n'est sélectionné, définir la valeur à NULL
+                $car = null;
+            } else {
+                $car = $detailsVoiture;
+            }
 
             $req = $mabd->prepare('INSERT INTO utilisateurs (user_nom, user_prenom, user_mail, user_mdp, user_genre, user_car) VALUES (:nom, :prenom, :email, :mdp, :genre, :car)');
             $req->execute(array(':nom' => $nom, ':prenom' => $prenom, ':email' => $email, ':mdp' => $mdp_hash, ':genre' => $genre, ':car' => $car));
 
-            session_start();
-            $_SESSION['email'] = $email;
+            $user = grab_user($mabd, $email);
+            if ($user) {
+                session_start();
+                $_SESSION['email'] = $user['user_mail'];
+            }
 
             header('Location: index.php?succes=1');
             exit();
@@ -41,8 +49,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     deconnexionBD($mabd);
 }
-
-require 'header.php';
 ?>
 
 <!DOCTYPE html>
@@ -53,37 +59,71 @@ require 'header.php';
 <body>
 
     <div id="inscription">
-        <h1>Inscription</h1>
-        <img src="img/avatar.png" alt="avatar">
-        <form action="" method="post">
-            <!-- Reste du formulaire -->
-        </form>
-        <p>Vous avez déjà un compte?</p>
-        <a href="connexion.php"><p>Connectez-vous!</p></a>
+      <h1>Inscription</h1>
+      <img src="img/avatar.png" alt="avatar">
+      <form action="" method="post">
+        <label for="prenom">Prénom*</label>
+        <input class="input" type="text" id="prenom" name="prenom" required><br><br>
+
+        <label for="nom">Nom*</label>
+        <input class="input" type="text" id="nom" name="nom" required><br><br>
+
+        <label for="genre">Pronoms*</label>
+        <select class="input" name="genre" id="genre" required>
+            <option value="il">Il</option>
+            <option value="elle">Elle</option>
+            <option value="iel">Iel</option>
+        </select><br><br>
+
+        <label for="email">Email*</label>
+        <input class="input" type="email" id="email" name="email" required><br><br>
+
+        <label for="mdp">Mot de passe*</label>
+        <input class="input" type="password" id="mdp" name="mdp" required><br><br>
+
+        <label for="voiture">Avez-vous une voiture ?*</label><br>
+        <div id="radio_voiture">
+          <div id="oui">
+            <input class="input" type="radio" name="voiture" value="oui" onclick="showTextField()">
+            <label for="voiture">Oui</label>
+          </div>
+          <div id="non">
+            <input class="input" type="radio" name="voiture" value="non" onclick="hideTextField()"><br>
+            <label for="voiture">Non</label>
+          </div>
+        </div>
+
+        <div id="detailsVoiture" style="display: none;">
+          <label for="detailsVoiture">Précisez la marque, la couleur et le modèle de votre voiture :*</label><br>
+          <input class="input" type="text" name="detailsVoiture" placeholder="Marque, couleur, modèle" required><br><br>
+        </div>
+
+        <div id="inscription_submit">
+          <input type="submit" value="En route!">
+        </div>
+
+      </form>
+      <p>Vous avez déjà un compte?</p>
+      <a href="connexion.php"><p>Connectez-vous!</p></a>
     </div>
 
-    <script>
-        // Fonctions JavaScript pour afficher/masquer le champ de détails de la voiture
-        function showTextField() {
-            document.getElementById('detailsVoiture').style.display = 'block';
-        }
+<script>
+  function showTextField() {
+    document.getElementById('detailsVoiture').style.display = 'block';
+  }
 
-        function hideTextField() {
-            document.getElementById('detailsVoiture').style.display = 'none';
-        }
-    </script>
+  function hideTextField() {
+    document.getElementById('detailsVoiture').style.display = 'none';
+  }
+</script>
 
-    <?php 
-    if (isset($_GET['erreur']) && $_GET['erreur'] == 1) {
-        echo "Cet email est déjà utilisé, veuillez vous connecter !";
-    } elseif (isset($_GET['succes']) && $_GET['succes'] == 1) {
-        echo "Inscription réussie !";
-    }
-    ?>
+<?php 
+if (isset($_GET['erreur']) && $_GET['erreur'] == 1) {
+    echo "Cet email est déjà utilisé, veuillez vous connecter !";
+} elseif (isset($_GET['succes']) && $_GET['succes'] == 1) {
+    echo "Inscription réussie !";
+}
+?>
 
 </body>
 </html>
-
-<?php 
-require 'footer.php';
-?>
