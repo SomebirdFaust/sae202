27eb0871f97f1fb2721fda211d6f350ae0347ad5
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-15 10:27:36 +0200

    corrections

--- supprProfil.php
+++ supprProfil.php
@@ -7,16 +7,32 @@
 </head>
 <body>
 <?php
-echo '<div id="modif_profil_supprimer">';
-    echo '<form action="supprProfilVerif.php" method="post">';
-    if (isset($user) && $user !== null) {
-        echo '<input type="hidden" name="user_id" value="' . $user['user_id'] . '">';
-    } else {
-        echo '<input type="hidden" name="user_id" value="">';
-    }
-    echo '</form>';
-echo '</div>';
-?>
+require 'admin/lib.inc.php';
 
+$user_id = $_POST['user_id'];
+
+try {
+    $mabd = connexionBD();
+    $req = $mabd->prepare('SELECT COUNT(*) as count FROM utilisateurs WHERE user_id = :user_id');
+    $req->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $req->execute();
+    $result = $req->fetch(PDO::FETCH_ASSOC);
+
+    if ($result['count'] > 0) {
+        echo '<div id="modif_profil_supprimer">';
+        echo '<form action="supprProfilVerif.php" method="post">';
+        echo '<input type="hidden" name="user_id" value="' . $user_id . '">';
+        echo '<input type="submit" value="Supprimer le compte">';
+        echo '</form>';
+        echo '</div>';
+    } else {
+        echo '<div>Utilisateur non trouvé.</div>';
+    }
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+
+deconnexionBD($mabd);
+?>
 </body>
 </html>
