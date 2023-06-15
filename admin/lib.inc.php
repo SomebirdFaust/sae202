ece9c995c9cbf81ee87a4e08121649f7dcb9a58e
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-15 12:02:34 +0200

    correction

--- admin/lib.inc.php
+++ admin/lib.inc.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
 // Vérifier si la connexion à la base de données est déjà établie, sinon la connecter
 if (!function_exists('connexionBD')) {
     function connexionBD()
@@ -22,30 +21,36 @@ if (!function_exists('deconnexionBD')) {
 }
 
 // Fonction pour déconnecter l'utilisateur
-function deconnecterUtilisateur() {
-    // Démarrer la session
+if (!function_exists('deconnecterUtilisateur')) {
+    function deconnecterUtilisateur()
+    {
+        // Démarrer la session
 
-    // Détruire toutes les variables de session
-    $_SESSION = array();
+        // Détruire toutes les variables de session
+        $_SESSION = array();
 
-    // Effacer le cookie de session
-    if (isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '', time()-42000, '/');
+        // Effacer le cookie de session
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time() - 42000, '/');
+        }
+
+        // Détruire la session
+        session_destroy();
     }
-
-    // Détruire la session
-    session_destroy();
 }
 
 // Fonction pour récupérer les informations de l'utilisateur connecté
-function grab_user($mabd) {
-    if (isset($_SESSION['user_id'])) {
-        $userId = $_SESSION['user_id'];
-        $req = $mabd->prepare('SELECT * FROM utilisateurs WHERE user_id = :user_id');
-        $req->execute(array(':user_id' => $userId));
-        $user = $req->fetch(PDO::FETCH_ASSOC);
-        return $user;
+if (!function_exists('grab_user')) {
+    function grab_user($mabd)
+    {
+        if (isset($_SESSION['user_id'])) {
+            $userId = $_SESSION['user_id'];
+            $req = $mabd->prepare('SELECT * FROM utilisateurs WHERE user_id = :user_id');
+            $req->execute(array(':user_id' => $userId));
+            $user = $req->fetch(PDO::FETCH_ASSOC);
+            return $user;
+        }
+        return null;
     }
-    return null;
 }
 ?>
