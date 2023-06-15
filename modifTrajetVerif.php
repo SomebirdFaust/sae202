a5d21be2efa9c3b851cef6b23db25ef602fcf766
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-15 23:36:36 +0200

    corrections

--- modifTrajetVerif.php
+++ modifTrajetVerif.php
@@ -33,7 +33,7 @@ try {
         exit();
     }
 
-    $requete = $mabd->prepare("UPDATE trajets SET _park_id = :depart_id, traj_arrivee = :dest_id, traj_date = :date, traj_heure_depart = :heure, traj_places = :places WHERE traj_id = :trajet_id");
+    $requete = $mabd->prepare("UPDATE trajets SET _park_id = :park_id, traj_arrivee = :dest_id, traj_date = :date, traj_heure_depart = :heure, traj_places = :places WHERE traj_id = :trajet_id");
     $requete->bindValue(':depart_id', $parking_depart['park_id'], PDO::PARAM_INT);
     $requete->bindValue(':dest_id', $parking_dest['park_id'], PDO::PARAM_INT);
     $requete->bindValue(':date', $date, PDO::PARAM_STR);
