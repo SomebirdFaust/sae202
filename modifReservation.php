1cee8d3dbb6025bbacad3b9800c1ef701790a648
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-15 13:17:43 +0200

    edits

--- modifReservation.php
+++ modifReservation.php
@@ -14,48 +14,45 @@ if(isset($_GET['reserv_id'])) {
     $reservation = $requeteReservation->fetch();
 
     if($reservation) {
-        // Vérifier si l'utilisateur est autorisé à modifier ou annuler la réservation
-        if($_SESSION['user_id'] == $reservation['_user_id']) {
-            // Récupérer le trajet correspondant à la réservation
-            $requeteTrajet = $mabd->prepare("SELECT * FROM trajets WHERE traj_id = :trajet_id");
-            $requeteTrajet->bindParam(':trajet_id', $reservation['_traj_id']);
-            $requeteTrajet->execute();
-            $trajet = $requeteTrajet->fetch();
+        // Récupérer le trajet correspondant à la réservation
+        $requeteTrajet = $mabd->prepare("SELECT * FROM trajets WHERE traj_id = :trajet_id");
+        $requeteTrajet->bindParam(':trajet_id', $reservation['_traj_id']);
+        $requeteTrajet->execute();
+        $trajet = $requeteTrajet->fetch();
 
-            if($trajet) {
-                // Vérifier si l'utilisateur souhaite augmenter le nombre de places réservées
-                if(isset($_POST['places_res'])) {
-                    $places_res = $_POST['places_res'];
+        if($trajet) {
+            // Vérifier si l'utilisateur souhaite augmenter le nombre de places réservées
+            if(isset($_POST['places_res'])) {
+                $places_res = $_POST['places_res'];
 
-                    // Vérifier si le nombre de places demandées est inférieur ou égal aux places disponibles
-                    if($places_res <= $trajet['traj_places']) {
-                        // Mettre à jour le nombre de places réservées dans la base de données
-                        $requeteModifier = $mabd->prepare("UPDATE reservations SET places_res = :places_res WHERE reservation_id = :reservation_id");
-                        $requeteModifier->bindParam(':places_res', $places_res);
-                        $requeteModifier->bindParam(':reserv_id', $reservation_id);
-                        $requeteModifier->execute();
+                // Vérifier si le nombre de places demandées est inférieur ou égal aux places disponibles
+                if($places_res <= $trajet['traj_places']) {
+                    // Mettre à jour le nombre de places réservées dans la base de données
+                    $requeteModifier = $mabd->prepare("UPDATE reservations SET places_res = :places_res WHERE reserv_id = :reserv_id");
+                    $requeteModifier->bindParam(':places_res', $places_res);
+                    $requeteModifier->bindParam(':reserv_id', $reservation_id);
+                    $requeteModifier->execute();
 
-                        echo "La réservation a été modifiée avec succès.";
-                    } else {
-                        echo "Nombre de places demandé supérieur aux places disponibles.";
-                    }
-                } elseif(isset($_POST['annuler'])) {
-                    // Annuler la réservation et libérer les places réservées
-                    $places_dispo = $trajet['traj_places'] + $reservation['places_res'];
-
-                    // Mettre à jour le nombre de places disponibles dans le trajet
-                    $requeteMajPlaces = $mabd->prepare("UPDATE trajets SET traj_places = :places_dispo WHERE traj_id = :trajet_id");
-                    $requeteMajPlaces->bindParam(':places_dispo', $places_dispo);
-                    $requeteMajPlaces->bindParam(':trajet_id', $trajet['traj_id']);
-                    $requeteMajPlaces->execute();
-
-                    // Supprimer la réservation de la base de données
-                    $requeteSupprimer = $mabd->prepare("DELETE FROM reservations WHERE reserv_id = :reserv_id");
-                    $requeteSupprimer->bindParam(':reserv_id', $reservation_id);
-                    $requeteSupprimer->execute();
-
-                    echo "La réservation a été annulée avec succès.";
+                    echo "La réservation a été modifiée avec succès.";
+                } else {
+                    echo "Nombre de places demandé supérieur aux places disponibles.";
                 }
+            } elseif(isset($_POST['annuler'])) {
+                // Annuler la réservation et libérer les places réservées
+                $places_dispo = $trajet['traj_places'] + $reservation['places_res'];
+
+                // Mettre à jour le nombre de places disponibles dans le trajet
+                $requeteMajPlaces = $mabd->prepare("UPDATE trajets SET traj_places = :places_dispo WHERE traj_id = :trajet_id");
+                $requeteMajPlaces->bindParam(':places_dispo', $places_dispo);
+                $requeteMajPlaces->bindParam(':trajet_id', $trajet['traj_id']);
+                $requeteMajPlaces->execute();
+
+                // Supprimer la réservation de la base de données
+                $requeteSupprimer = $mabd->prepare("DELETE FROM reservations WHERE reserv_id = :reserv_id");
+                $requeteSupprimer->bindParam(':reserv_id', $reservation_id);
+                $requeteSupprimer->execute();
+
+                echo "La réservation a été annulée avec succès.";
             }
         }
     } else {
