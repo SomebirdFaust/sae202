1b68e11e98dae9219c42076d0d2b734c259888b3
SomebirdFaust <faust.gaulard@gmail.com>
2023-06-16 02:41:04 +0200

    suppr le code zarb

--- admin/supprParking.php
+++ admin/supprParking.php
@@ -22,29 +22,6 @@ $req = 'DELETE FROM parkings WHERE park_id='. $num;
 
 echo '<h2>Vous venez de supprimer le '.$park_nom.'.</h2>';
 
-$resultat = $mabd->query($req);
-
-try {
-    $mabd = connexionBD();
-    $req = $mabd->prepare('SELECT COUNT(*) as count FROM utilisateurs WHERE user_id = :user_id');
-    $req->execute(array(':user_id' => $user_id));
-    $result = $req->fetch(PDO::FETCH_ASSOC);
-
-    if ($result['count'] > 0) {
-        $req = $mabd->prepare('DELETE FROM utilisateurs WHERE user_id = :user_id');
-        $req->execute(array(':user_id' => $user_id));
-
-        header('location: ../index.php?deleted=1');
-        exit();
-    } else {
-        echo "Utilisateur non trouvé !";
-        header('location: ../modifProfil.php?erreur=1');
-        exit();
-    }
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
-
 
 ?>
 
