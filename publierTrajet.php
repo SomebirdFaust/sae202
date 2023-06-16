627630f164d5e292a3fb23be844d060af91f4476
LeaneBerlo <leane.berlo@gmail.com>
2023-06-16 03:44:22 +0200

    publier trajet ok

--- publierTrajet.php
+++ publierTrajet.php
@@ -13,47 +13,49 @@ require 'header.php';
 <body>
     <div id="publier_trajet">
         <form action="publierTrajetVerif.php" method="POST">
-            <label for="depart">Départ</label>
-            <select class="input" name="depart" id="depart" required>
+            <div id="champs">
+                <label for="depart">Départ</label><br>
+                <select class="input input_pc" name="depart" id="depart" required>
+                    <?php
+                    $mabd = connexionBD();
+                    $requete = $mabd->query("SELECT park_nom FROM parkings");
+                    while ($park = $requete->fetch()) {
+                        $parkingNom = $park['park_nom'];
+                        echo "<option value='$parkingNom'>$parkingNom</option>";
+                    }
+                    ?>
+                </select><br><br>
+
+                <label for="nom">Destination</label><br>
+                <input class="input input_pc" type="text" id="dest" name="dest" required placeholder="Ville"><br><br>
+
+                <label for="date">Date</label><br>
+                <input class="input input_pc" type="date" id="date" name="date" required><br><br>
+
+                <label for="heure">Heure de départ</label><br>
+                <input class="input input_pc" type="time" id="heure" name="heure" required><br><br>
+
+                <label for="places">Nombre de places</label><br>
+                <select class="input input_pc" name="places" id="places" required>
+                    <?php
+                    for ($i = 1; $i <= 9; $i++) {
+                        echo "<option value='$i'>$i</option>";
+                    }
+                    ?>
+                </select><br><br>
+
                 <?php
-                $mabd = connexionBD();
-                $requete = $mabd->query("SELECT park_nom FROM parkings");
-                while ($park = $requete->fetch()) {
-                    $parkingNom = $park['park_nom'];
-                    echo "<option value='$parkingNom'>$parkingNom</option>";
+                if (isset($_GET['message'])) {
+                    urldecode($_GET['message']);
+                    echo '<div id="publier_trajet_voiture">';
+                    echo "<p>Vous n'avez pas de voiture</p>";
+                    echo '</div>';
                 }
                 ?>
-            </select><br><br>
 
-            <label for="nom">Destination</label>
-            <input class="input" type="text" id="dest" name="dest" required placeholder="Ville"><br><br>
-
-            <label for="date">Date</label>
-            <input class="input" type="date" id="date" name="date" required><br><br>
-
-            <label for="heure">Heure de départ</label>
-            <input class="input" type="time" id="heure" name="heure" required><br><br>
-
-            <label for="places">Nombre de places</label>
-            <select class="input" name="places" id="places" required>
-                <?php
-                for ($i = 1; $i <= 9; $i++) {
-                    echo "<option value='$i'>$i</option>";
-                }
-                ?>
-            </select><br><br>
-
-            <?php
-            if (isset($_GET['message'])) {
-                urldecode($_GET['message']);
-                echo '<div id="publier_trajet_voiture">';
-                echo "<p>Vous n'avez pas de voiture</p>";
-                echo '</div>';
-            }
-            ?>
-
-            <div id="publier_trajet_submit">
-                <input type='submit' value='Publier le trajet'>
+                <div id="publier_trajet_submit">
+                    <input type='submit' value='Publier le trajet'>
+                </div>
             </div>
             
             <div id="publier_trajet_img">
